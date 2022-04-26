825bbaf46e22c141dc3d9c54b734a68b25768601
SodaCris <wang-jiaxun@outlook.com>
2022-04-26 16:34:43 +0800

    add audit hook

--- audit.py
+++ audit.py
@@ -1,13 +1,25 @@
 import sys
 
+
 def audit_hook(event, args):
-    if event in ['urllib.Request']:
-        print(f"Network {event=} {args=}")
+    if event in ["urllib.Request", "os.remove"]:
+        print(f"{event=} {args=}")
+        if event == "os.remove":
+            sys.exit(-1)
+
 
 sys.addaudithook(audit_hook)
 
 import urllib.request
+
 try:
     urllib.request.urlopen("http://example.com")
 except:
     pass
+
+with open("file.txt", mode="w") as f:
+    f.write("hello world\n")
+
+from os import remove
+
+remove("file.txt")
